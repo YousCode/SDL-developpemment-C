6f60f10dccb228012329165985624b8873e9c745
PAPA Youssouph Camara <88164361+YousCode@users.noreply.github.com>
2021-09-01 15:06:34 +0200

    Delete SDL2/DVC_COURS directory

--- SDL2/DVC_COURS/include/init.h
+++
@@ -1,8 +0,0 @@
-#ifndef INIT
-#define INIT
-
-#include "window.h"
-
-int initSDL(Win *app);
-
-#endif /* !INIT */
--- SDL2/DVC_COURS/include/input.h
+++
@@ -1,8 +0,0 @@
-#ifndef INPUT
-#define INPUT
-
-#include <SDL2/SDL.h>
-#include"draw.h"
-
-int input_handler();
-#endif /*INPUT */
